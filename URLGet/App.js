24a812f87e02dc1a9238684c82a411917851be42
ardagon89 <shariq.mellon@gmail.com>
2021-01-21 13:56:54 -0600

    Local push notification scheduled

--- URLGet/App.js
+++ URLGet/App.js
@@ -1,21 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import React, { Component } from "react";
+import React, { Component, useState, useEffect, useRef } from "react";
 import {
   StyleSheet,
   Button,
   View,
   SafeAreaView,
   Text,
+  TextInput,
+  TouchableOpacity,
   Alert,
+  Platform,
 } from "react-native";
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
 
 const Separator = () => <View style={styles.separator} />;
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = { leftText: "blank!" };
-  }
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      }
+    );
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { leftText: "blank!" };
+  // }
 
   leftButtonAction = () => {
     fetch(
@@ -23,34 +64,109 @@ export default class App extends Component {
     )
       .then((response) => response.json())
       .then((data) => {
-        this.setState({ leftText: data["tranMessage"] });
+        console.log(data);
+        // this.setState({ leftText: data["tranMessage"] });
       })
       .catch((error) => console.log(error));
   };
-  render() {
-    console.log("App executed!");
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Text style={styles.title}>{this.state.leftText}</Text>
-        </View>
-        <Separator />
-        <View>
-          <Text style={styles.title}>
-            This layout strategy lets the title define the width of the button.
+  // render() {
+  console.log("App executed!");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>{"Some text!"}</Text>
+      </View>
+      <Separator />
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <Text>Your expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Text>
+            Title: {notification && notification.request.content.title}{" "}
           </Text>
-          <View style={styles.fixToText}>
-            <Button title="Get" onPress={this.leftButtonAction} />
-            <Button
-              title="Right button"
-              onPress={() => Alert.alert("Right button pressed")}
-            />
-          </View>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>
+            Data:{" "}
+            {notification && JSON.stringify(notification.request.content.data)}
+          </Text>
         </View>
-      </SafeAreaView>
-    );
+        <Button
+          title="Press to schedule a notification"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          This layout strategy lets the title define the width of the button.
+        </Text>
+        <View style={styles.fixToText}>
+          <Button
+            title="Get"
+            onPress={() => Alert.alert("Get button pressed")}
+          />
+          <Button
+            title="Right button"
+            onPress={() => Alert.alert("Right button pressed")}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+  // }
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const {
+      status: existingStatus,
+    } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
   }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
 }
 
 const styles = StyleSheet.create({

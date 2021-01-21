190b284cdfa2c734aa211eb391973a96a457403a
ardagon89 <shariq.mellon@gmail.com>
2021-01-21 11:13:55 -0600

    Implemented lamdba fetch on button press

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # react-native-URLGet-app
+
 React native app to use API methods and lambda funtions
+
+# If running in docker
+
+docker-compose up --build
+<br/>
+docker-compose down --remove-orphans
+<br/>
+docker-compose push
+
+# Find the web app at
+
+http://192.168.99.100:19006/
+
+# If running in Android Studio
+
+npm install -g expo-cli
+<br/>
+expo init URLGet --template blank # Also: npx URLGet run-android
+<br/>
+cd URLGet
+<br/>
+npm start
+<br/>
+
+# To run your project, navigate to the directory and run one of the following npm commands.
+
+- cd URLGet
+- npm start # you can open iOS, Android, or web from here, or run them directly with the commands below.
+- npm run android
+- npm run ios # requires an iOS device or macOS for access to an iOS simulator
+- npm run web
--- URLGet/App.js
+++ URLGet/App.js
@@ -1,25 +1,75 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from "react-native";
 
-export default function App() {
-  console.log("App executed!");
-  let x;
-  x.toString();
+const Separator = () => <View style={styles.separator} />;
 
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { leftText: "blank!" };
+  }
+
+  leftButtonAction = () => {
+    fetch(
+      "https://5ketfr2si7.execute-api.us-east-2.amazonaws.com/dev/transactions?tranId=2&tranType=PURCHASE&tranAmount=100"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ leftText: data["tranMessage"] });
+      })
+      .catch((error) => console.log(error));
+  };
+  render() {
+    console.log("App executed!");
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}>{this.state.leftText}</Text>
+        </View>
+        <Separator />
+        <View>
+          <Text style={styles.title}>
+            This layout strategy lets the title define the width of the button.
+          </Text>
+          <View style={styles.fixToText}>
+            <Button title="Get" onPress={this.leftButtonAction} />
+            <Button
+              title="Right button"
+              onPress={() => Alert.alert("Right button pressed")}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
